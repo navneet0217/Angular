eabb7aadfd8c310101d8e7b2c614b5df7a241dd6
navneet0217 <navneetanand17360@gmail.com>
2026-05-11 18:02:48 +0530

    1) How to Create Component in Angular 2) What are Standalone components 3) What is Component Decorator

---
+++ angular_18_tutorial/src/app/components/employee-list/employee-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './employee-list.component.html',
+  styleUrls: ['./employee-list.component.css']
+})
+export class EmployeeListComponent {
+
+}
